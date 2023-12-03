1896d4f2eb09048113bebdc717c7867176d41477
JuanUbaldi <106642655+JuanUbaldi@users.noreply.github.com>
2023-12-02 23:32:31 -0300

    commit

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -3,7 +3,6 @@ import style from "./Technologies.module.css";
 
 import NavBar from "../NavBar/NavBar";
 
-
 const Technologies = () => {
   return (
     <>
@@ -12,7 +11,21 @@ const Technologies = () => {
           <NavBar />
         </div>
         <div className={style.techsSingle}>
-       
+          <img src="./redux.svg" width={120} height={120} alt="" />
+          <img src="./javascript.svg" width={120} height={120} alt="" />
+          <img src="./next.svg" width={120} height={120} alt="" />
+          <img src="./scrum.svg" width={120} height={120} alt="" />
+          <img src="./mongo.svg" width={120} height={120} alt="" />
+          <img src="./tailwind.svg" width={120} height={120} alt="" />
+          <img src="./bootstrap.svg" width={120} height={120} alt="" />
+          <img src="./figma.svg" width={120} height={120} alt="" />
+          <img src="./git.svg" width={120} height={120} alt="" />
+
+          <img src="./sql.ico" width={120} height={120} alt="" />
+          <img src="./react.svg" width={120} height={120} alt="" />
+          <img src="./css.svg" width={120} height={120} alt="" />
+          <img src="./html.svg" width={120} height={120} alt="" />
+          <img src="./express.svg" width={120} height={120} alt="" />
         </div>
       </div>
     </>
